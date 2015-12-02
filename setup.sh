51b33773a8338ff0e237780227d95b270ab4b292
Dominykas Djacenko <chaosteil@gmail.com>
2015-12-02 10:51:08 +0200

    Updated i3 configs and fixed zsh setup.

--- setup.sh
+++ setup.sh
@@ -28,13 +28,12 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 rm $HOME/.zshrc
 ln -s $PWD/zshrc $HOME/.zshrc
 ln -s $PWD/zsh/oh-my-zsh/themes/chaosteil.zsh-theme $HOME/.oh-my-zsh/themes/chaosteil.zsh-theme
-rm -r $PWD/zsh/oh-my-zsh/custom/plugins
-ln -s $PWD/zsh/oh-my-zsh/custom/plugins $HOME/.oh-my-zsh/custom/plugins
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s $PWD/zsh/oh-my-zsh/custom/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+ln -s $PWD/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 touch $HOME/.local_paths
 
 # terminal settings
 rm -r $CONFIG/i3
-ln -s $PWD/i3/config $CONFIG/i3/config
+ln -s $PWD/i3 $CONFIG/i3
 rm -r $CONFIG/terminator
-ln -s $PWD/terminator/config $CONFIG/terminator/config
+ln -s $PWD/terminator $CONFIG/terminator
