2f5dae43d80f4378e98eea82686964e1c48cd54e
Dominykas Djacenko <chaosteil@gmail.com>
2017-01-02 22:46:43 +0200

    Use stow for more configs

--- setup.sh
+++ setup.sh
@@ -18,12 +18,9 @@ ln -s $cwd/zsh/oh-my-zsh/custom/plugins/zsh-syntax-highlighting $home/.oh-my-zsh
 touch $home/.local_paths
 
 stow i3
-rm -r $config/termite
-rm -r $config/ranger
-rm -r $config/dunst
-ln -s $cwd/ranger $config/ranger
-ln -s $cwd/termite $config/termite
-ln -s $cwd/dunst $config/dunst
+stow termite
+stow ranger
+stow dunst
 ln -s $cwd/Xresources $home/.Xresources
 ln -s $cwd/xinitrc $home/.xinitrc
 ln -s $cwd/gtkrc-2.0 $home/.gtkrc-2.0
